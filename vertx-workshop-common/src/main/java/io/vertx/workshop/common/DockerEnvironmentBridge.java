6f4d4a7215246bbc9afed444db215809051d9bcd
Clement Escoffier <clement.escoffier@gmail.com>
2016-04-11 11:54:27 +0200

    Test the Docker link imports

--- vertx-workshop-common/src/main/java/io/vertx/workshop/common/DockerEnvironmentBridge.java
+++ vertx-workshop-common/src/main/java/io/vertx/workshop/common/DockerEnvironmentBridge.java
@@ -5,12 +5,15 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.discovery.DiscoveryService;
 import io.vertx.ext.discovery.Record;
 import io.vertx.ext.discovery.spi.DiscoveryBridge;
 import io.vertx.ext.discovery.spi.ServiceType;
 import io.vertx.ext.discovery.types.HttpEndpoint;
 import io.vertx.ext.discovery.types.HttpLocation;
+import sun.rmi.runtime.Log;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -21,17 +24,19 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
+ * A discovery bridge importing services from the environment variables exported by Docker when using links.
+ *
  * @author <a href="http://escoffier.me">Clement Escoffier</a>
  */
 public class DockerEnvironmentBridge implements DiscoveryBridge {
-  private Vertx vertx;
   private DiscoveryService discovery;
 
   private List<Record> records = new ArrayList<>();
 
+  private final static Logger LOGGER = LoggerFactory.getLogger(DockerEnvironmentBridge.class);
+
   @Override
   public void start(Vertx vertx, DiscoveryService discovery, JsonObject configuration, Handler<AsyncResult<Void>> completionHandler) {
-    this.vertx = vertx;
     this.discovery = discovery;
 
     synchronized (this) {
@@ -49,29 +54,26 @@ public class DockerEnvironmentBridge implements DiscoveryBridge {
         .filter(key -> key != null)
         .collect(Collectors.toList());
 
-    System.out.println("Found links: " + links);
+    LOGGER.info("Docker links: " + links);
 
     for (String link : links) {
       try {
         Record record = createRecord(link,
             variables.entrySet().stream()
                 .filter(entry -> entry.getKey().startsWith(link + "_"))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)
-                ));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
 
-        System.out.println("Record created : " + record);
+        LOGGER.info("Record created from link " + link + " : " + record);
         discovery.publish(record, ar -> {
           if (ar.succeeded()) {
             records.add(ar.result());
-            System.out.println("Service imported from Docker link : " + link + " : "
+            LOGGER.info("Service imported from Docker link : " + link + " with endpoint set to "
                 + ar.result().getLocation().getString(Record.ENDPOINT));
           } else {
-            System.out.println("Publication of the docker link " + link + " as a service failed");
-            ar.cause().printStackTrace();
+            LOGGER.error("Publication of the docker link " + link + " as a service failed", ar.cause());
           }
         });
       } catch (URISyntaxException e) {
-        e.printStackTrace();
         if (completionHandler != null) {
           completionHandler.handle(Future.failedFuture(e));
         } else {
@@ -87,17 +89,13 @@ public class DockerEnvironmentBridge implements DiscoveryBridge {
 
   private String extractLinkName(String key, Map<String, String> variables) {
     // We know it ends with _NAME;
-    System.out.println("Is " + key + " as link ?");
     String name = key.substring(0, key.length() - "_NAME".length());
-    System.out.println("Is " + key + " as link ? = " + name);
     if (name.isEmpty()) {
       // Weird case the key is just _NAME
       return null;
     } else {
       String port = name + "_PORT";
-      System.out.println(port);
       if (variables.containsKey(port)) {
-        System.out.println("OK !");
         return name;
       } else {
         // Not a valid link
@@ -139,7 +137,7 @@ public class DockerEnvironmentBridge implements DiscoveryBridge {
     if (type == null) {
       type = ServiceType.UNKNOWN;
     } else {
-      System.out.println("Service type for " + name + " : " + type);
+      LOGGER.info("Service type for " + name + " : " + type);
     }
 
     URI url = new URI(variables.get(name + "_PORT"));
