30f2dff4f1b7c3eafd3246c1cbecab7f633e517d
Clement Escoffier <clement.escoffier@gmail.com>
2017-03-07 16:58:47 +0100

    create the output directory if needed

--- docs/build.sh
+++ docs/build.sh
@@ -20,6 +20,7 @@ MASTER_ADOC=index.adoc
 MASTER_DOCBOOK=${MASTER_ADOC/.adoc/.xml}
 
 # Command options
+mkdir -p output
 SHARED_OPTIONS='-a numbered -a experimental -a source-highlighter=coderay -r asciidoctor-diagram -a imagesdir=images
 --destination-dir=output'
 
